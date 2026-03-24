6d58e3fa8078b42a4bf9107a7ccbb63f44286047
smeisegeier <dexterDSD@googlemail.com>
2026-03-24 21:51:20 +0100

    sys-open, githead

--- init.lua
+++ init.lua
@@ -14,60 +14,10 @@ if os.getenv("OSTYPE") == "linux-gnu" then
   require("sshfs"):setup()
 end
 
-
 require("githead"):setup({
-	order = {
-		"__spacer__",
-		"stashes",
-		"__spacer__",
-		"state",
-		"__spacer__",
-		"staged",
-		"__spacer__",
-		"unstaged",
-		"__spacer__",
-		"untracked",
-		"__spacer__",
-		"branch",
-		"remote_branch",
-		"__spacer__",
-		"tag",
-		"__spacer__",
-		"commit",
-		"__spacer__",
-		"behind_ahead_remote",
-		"__spacer__",
-	},
-
-	branch_borders = "{}",
-	branch_prefix = "|",
-	branch_color = "#7aa2f7",
-	remote_branch_color = "#9ece6a",
-	always_show_remote_branch = true,
-	always_show_remote_repo = true,
-
-	tag_symbol = "󰓼",
-	always_show_tag = true,
-	tag_color = "#bb9af7",
-
-	commit_symbol = "",
-	always_show_commit = true,
-	commit_color = "#e0af68",
-
-	staged_color = "#73daca",
-	staged_symbol = "●",
-
-	unstaged_color = "#e0af68",
-	unstaged_symbol = "✗",
-
-	untracked_color = "#f7768e",
-	untracked_symbol = "?",
-
-	state_color = "#f5c359",
-	state_symbol = "󱐋",
-
-	stashes_color = "#565f89",
-	stashes_symbol = "⚑",
+  branch_prefix = "on",
+  branch_symbol = " ",
+  branch_borders = "()",
 })
 
 require("copy-file-contents"):setup({
